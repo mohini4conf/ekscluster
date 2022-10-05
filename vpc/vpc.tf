3b96fe836a27b7e9f8d2142219525dc0188509bc
U-BHI-MASTER\upasmoh <upasmoh@BHIH59HSQ3.ent.bhicorp.com>
2022-10-05 10:48:31 +0530

    modify tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,20 +1,19 @@
-resource "aws_vpc" "prod-vpc" {
+resource "aws_vpc" "main-vpc" {
 cidr_block = "10.0.0.0/16"
 enable_dns_support = "true" #gives you an internal domain name
 enable_dns_hostnames = "true" #gives you an internal host name
-enable_classiclink = "false"
 instance_tenancy = "default"
 
-tags {
-Name = "prod-vpc"
+tags =  {
+Name = "main-vpc"
 }
 }
 resource "aws_subnet" "prod-subnet-public-1" {
-vpc_id = "${aws_vpc.prod-vpc.id}"
+vpc_id = "${aws_vpc.main-vpc.id}"
 cidr_block = "10.0.1.0/24"
 map_public_ip_on_launch = "true" //it makes this a public subnet
 availability_zone = "ap-south-1a"
-tags {
+tags =  {
 Name = "prod-subnet-public-1"
 }
 }
