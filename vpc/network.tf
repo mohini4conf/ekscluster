de3b680fd9955c3e4d374662521b519b7328b063
mohini4conf <mohini4conf@gmail.com>
2022-10-05 16:35:44 +0530

    add one more subnet

--- vpc/network.tf
+++ vpc/network.tf
@@ -22,6 +22,11 @@ resource "aws_route_table_association" "prod-crta-public-subnet-1"{
   route_table_id = "${aws_route_table.prod-public-crt.id}"
 }
 
+resource "aws_route_table_association" "prod-crta-public-subnet-2"{
+  subnet_id = "${aws_subnet.prod-subnet-public-2.id}"
+  route_table_id = "${aws_route_table.prod-public-crt.id}"
+}
+
 resource "aws_security_group" "ssh-allowed" {
   vpc_id = "${aws_vpc.main-vpc.id}"
 
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -17,3 +17,13 @@ tags =  {
 Name = "prod-subnet-public-1"
 }
 }
+
+resource "aws_subnet" "prod-subnet-public-2" {
+  vpc_id = "${aws_vpc.main-vpc.id}"
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true" //it makes this a public subnet
+  availability_zone = "ap-south-1b"
+  tags =  {
+    Name = "prod-subnet-public-2"
+  }
+}
